d347fe42d76d816051ac579086a7251f98da5636
bryful <bryful@gmail.com>
2020-03-29 20:33:17 +0900

    FootageBrowserの作成開始

---
+++ AE_FootageBrowser/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using BRY;
+
+using Codeplex.Data;
+/// <summary>
+/// 基本となるアプリのスケルトン
+/// </summary>
+namespace AE_Util_skelton
+{
+    public partial class Form1 : Form
+    {
+        NavBar m_navBar = new NavBar();
+        //-------------------------------------------------------------
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            NavBarSetup();
+        }
+        //-------------------------------------------------------------
+        private void NavBarSetup()
+        {
+            m_navBar.Form = this;
+            m_navBar.SizeSet();
+            m_navBar.LocSet();
+            m_navBar.Show();
+
+        }
+        /// <summary>
+        /// コントロールの初期化はこっちでやる
+        /// </summary>
+        protected override void InitLayout()
+        {
+            base.InitLayout();
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// フォーム作成時に呼ばれる
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //設定ファイルの読み込み
+            JsonPref pref = new JsonPref();
+            if (pref.Load())
+            {
+                bool ok = false;
+                Size sz = pref.GetSize("Size", out ok);
+                if (ok) this.Size = sz;
+                Point p = pref.GetPoint("Point", out ok);
+                if (ok) this.Location = p;
+            }
+            this.Text = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            m_navBar.Caption = this.Text;
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// フォームが閉じられた時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //設定ファイルの保存
+            JsonPref pref = new JsonPref();
+            pref.SetSize("Size", this.Size);
+            pref.SetPoint("Point", this.Location);
+            pref.Save();
+
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// ドラッグ＆ドロップの準備
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        /// <summary>
+        /// ドラッグ＆ドロップの本体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ここでは単純にファイルをリストアップするだけ
+            GetCommand(files);
+        }
+        //-------------------------------------------------------------
+        /// <summary>
+        /// ダミー関数
+        /// </summary>
+        /// <param name="cmd"></param>
+        public void GetCommand(string[] cmd)
+        {
+            if (cmd.Length > 0)
+            {
+                foreach (string s in cmd)
+                {
+                    listBox1.Items.Add(s);
+                }
+            }
+        }
+        /// <summary>
+        /// メニューの終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        //-------------------------------------------------------------
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AppInfoDialog.ShowAppInfoDialog();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            JsonPref j = new JsonPref();
+
+            int[] aaa = new int[] { 78, 9, 12 };
+            double[] bbb = new double[] { 0.7, 0.01, 0.12 };
+            string[] ccc = new string[] { "eee", "sfskjbF", "13" };
+            j.SetIntArray("aa", aaa);
+            j.SetDoubleArray("bb", bbb);
+            j.SetStringArray("cc", ccc);
+
+            MessageBox.Show(j.ToJson());
+
+        }
+
+
+        /*
+private void button1_Click(object sender, EventArgs e)
+{
+	dynamic a = new DynamicJson();
+	a.fff = new string[] { "a", "B" };
+	a.fff = "12";
+	//a.fff = new { aaa=12, ccc="www" };
+
+	MessageBox.Show(a.fff.GetType().ToString());
+
+	JsonPref s = new JsonPref();
+	s.AddInt("aaa", 99);
+	string ss = s.ToJson();
+	MessageBox.Show(ss);
+	s.Parse(ss);
+	string sss = s.ToJson();
+	MessageBox.Show(sss);
+
+	int i = s.GetInt("aaa");
+	MessageBox.Show(String.Format("{0}", i));
+}
+*/
+    }
+}
---
+++ AE_FootageBrowser/SequenceFIleName.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AE_FootageBrowser
+{
+    public class SequenceFileName
+    {
+        private string m_Parent = "";
+        private string m_Node = "";
+        private string m_Frame = "";
+        private string m_Ext = "";
+        private int m_StartFrame = 0;
+        private int m_LastFrame = 0;
+        public SequenceFileName()
+        {
+
+        }
+        public SequenceFileName(string s)
+        {
+
+        }
+        static public string[] Analysis(string s)
+        {
+            string[] ret = new string[4];
+            ret[0] = ret[1] = ret[2] = ret[3] = "";
+            if (s == "") return ret;
+            string p = Path.GetDirectoryName(s);
+            string e = Path.GetExtension(s);
+            string tmp = Path.GetFileNameWithoutExtension(s);
+
+            string n = "";
+            string f = "";
+
+            int idx = -1;
+            for ( int i= tmp.Length-1; i>=0;i--)
+            {
+                char c = tmp[i];
+                if ((c < '0') || (c > '9'))
+                {
+                    idx = i;
+                    break;
+                }
+            }
+
+            //fff001
+            //012345
+            if (idx <= 0)
+            {
+                //全部数字
+                f = tmp;
+            }else if (idx== tmp.Length - 1)
+            {
+                //数字なし
+                n = tmp;
+            }
+            else
+            {
+                n = tmp.Substring(0, idx + 1);
+                f = tmp.Substring(idx + 1);
+            }
+            ret[0] = p;
+            ret[1] = n;
+            ret[2] = f;
+            ret[3] = e;
+
+            return ret;
+        }
+
+    }
+}
