05ba49266793e4a1ab0c3853a1441adf9dd0b2f9
bryful <bryful@gmail.com>
2021-03-08 16:07:28 +0900

    readme.md

---
+++ Alert/README.md
@@ -0,0 +1,51 @@
+﻿# Alert.exe
+
+After Effects 2020のスクリプトで表示されるアラートダイアログがあまりにも見づらいので作ったものです。
+
+* htmlタグを認識します。cssも大丈夫と思います。
+* 読み込み時に上書き・追加が選べます。
+
+
+# 使い方
+
+scriptCode/AlertSample.jsxを参照してください。<br>
+
+基本的には
+
+```
+var mes = {};
+	mes.headcss = "p{color:blue; font-size:large;}h1{color:red; line-height:1.5; font-size:xx-large;}body{font-family:'メイリオ')}";
+	mes.title = title;
+	mes.body = "<h1>Alert Sample</h1>";
+	mes.body += "<p>アラートダイアログのサンプルです。<br>";
+	mes.body += "こんなことができます";
+	mes.body += "<lu><li><b>html</b>のタグが使えます。<li>というかhtmlしか使えない<li>cssも使えるはず</lu>";
+	mes.body += "</lu></p>";
+	//mes.left = 50; // isCenterがtrueだと無視される
+	//mes.top = 50; // isCenterがtrueだと無視される
+	//mes.width = 450;
+	//mes.height = 400;
+	mes.isCenter = false;
+	var objStr = mes.toSource();
+```
+上記のオブジェクトを作成しtoSourceでテキスト化してファイルに保存。それをalert.exeに読み込ませば表示されます。<br>
+<br>
+**system.callSystem**を使ってAlert.exe呼び出すと通常のダイアログっぽい動作になります。**AlertCall.exe**を使って呼び出すとフローティングパレットのような挙動になります。
+
+
+# Dependency
+Visual studio 2019 C#
+
+
+## Setup
+
+## License
+This software is released under the MIT License, see LICENSE
+
+## Authors
+
+bry-ful(Hiroshi Furuhashi)
+twitter:[bryful](https://twitter.com/bryful)
+bryful@gmail.com
+
+## References
---
+++ Alert/scriptCode/AlertSample.jsx
@@ -0,0 +1,82 @@
+﻿(function (){
+
+    /*
+        alert.exeを呼び出すサンプル
+    */
+    var alertFunc = function(body,title,css)
+    {
+        //オブジェクトに以下のパラメータを設定
+        var mes = {};
+        if ( (css!=undefined)&&(css!=null)&&(css!=""))
+        {
+            mes.headcss = css;
+        }
+        else
+        {
+            mes.headcss = "p{color:blue; font-size:large;}h1{color:red; line-height:1.5; font-size:xx-large;}body{font-family:'メイリオ')}";
+        }
+        if ( (title!=undefined)&&(title!=null)&&(title!=""))
+        {
+            mes.title = title;
+        }else{
+            mes.title = "alert サンプル";
+        }
+        if ( (body!=undefined)&&(body!=null)&&(body!=""))
+        {
+            mes.body = body;
+        }else{
+            mes.body = "<h1>Alert Sample</h1>";
+            mes.body += "<p>アラートダイアログのサンプルです。<br>";
+            mes.body += "こんなことができます";
+            mes.body += "<lu><li><b>html</b>のタグが使えます。<li>というかhtmlしか使えない<li>cssも使えるはず</lu>";
+            mes.body += "</lu></p>";
+
+        }
+
+        //mes.left = 50; // isCenterがtrueだと無視される
+        //mes.top = 50; // isCenterがtrueだと無視される
+        //mes.width = 450;
+        //mes.height = 400;
+        mes.isCenter = false;
+        var objStr = mes.toSource();
+
+        //tempフォルダにファイルを保存 拡張子は.json
+        var f = new File(Folder.temp.fullName +"/alert.json");
+
+        //一応実行前に同盟ファイルは消しておく
+        if (f.exists){
+            f.remove();
+        }
+        if (f.open("w"))
+        {
+            try{
+                //エンコードをutf-8にしておく
+                f.encoding = "utf-8";
+                f.write(objStr);
+            }catch(e){
+
+            }finally{
+                f.close();
+
+            }
+        }
+        if (f.exists)
+        {
+            try{
+                //alert.exeの場所は任意に。
+                //ここではスクリプトと同じ場所として記述
+                //渡すパスはfsNameで作成
+                var call = "alertcall.exe"; // フローティングダイアログ
+                //var call = "alert.exe"; // 普通のダイアログ
+                call +=" \"" +f.fsName +"\""
+                system.callSystem(call);
+            }catch(e){
+                alert(e.toString());
+            }
+        }
+
+    }
+
+    alertFunc("アラート表示のサンプルA<br>","サンプル");
+
+})();
